f6babe20f5d2086039c5224d9ff5368a7c41a094
Malarmathi-r04 <malarrmathie20@gmail.com>
2026-09-05 15:19:12 +0530

    PIR sensor code to detect motion

---
+++ Activity-05-PIR-Sensor/code.ino
@@ -0,0 +1,28 @@
+int pirPin = 2;
+int ledPin = 13;
+
+int motionState;
+
+void setup() {
+  pinMode(pirPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  motionState = digitalRead(pirPin);
+
+  if (motionState == HIGH) {
+    digitalWrite(ledPin, HIGH);
+
+    Serial.println("Motion Detected");
+  } 
+  else {
+    digitalWrite(ledPin, LOW);
+
+    Serial.println("No Motion");
+  }
+
+  delay(500);
+}
