5adbfa5785c8dfbdea6772c0e465f3d24178fb3a
Malarmathi-r04 <malarrmathie20@gmail.com>
2026-09-05 15:05:35 +0530

    push button control for LED
    Refactored code to use a push button for LED control instead of a fixed blinking pattern.

--- Activity-02_Push_Button/code.ino
+++ Activity-02_Push_Button/code.ino
@@ -1,14 +1,20 @@
-// C++ code
-//
-void setup()
-{
-  pinMode(LED_BUILTIN, OUTPUT);
+const int BUTTON_PIN = 2; // Digital pin connected to the pushbutton
+const int LED_PIN = 13;   // Digital pin connected to the LED
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+  // Activates the internal resistor, pulling the pin HIGH by default.
+  // The button must connect pin 2 directly to Ground (GND).
+  pinMode(BUTTON_PIN, INPUT_PULLUP); 
 }
 
-void loop()
-{
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000); // Wait for 1000 millisecond(s)
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(1000); // Wait for 1000 millisecond(s)
+void loop() {
+  int buttonState = digitalRead(BUTTON_PIN);
+
+  // Inverted logic: when the button is pressed, the pin connects to GND (LOW)
+  if (buttonState == LOW) {
+    digitalWrite(LED_PIN, HIGH);  // Turn LED on
+  } else {
+    digitalWrite(LED_PIN, LOW);   // Turn LED off
+  }
 }
