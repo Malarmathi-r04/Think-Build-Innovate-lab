7fda8886c05ca187dc7d188464b0a7a5d68d804a
Malarmathi-r04 <malarrmathie20@gmail.com>
2026-09-05 15:04:04 +0530

    LED blinking with potentiometer reading

--- Activity-03-Potentiometer/code.ino
+++ Activity-03-Potentiometer/code.ino
@@ -1,14 +1,14 @@
-// C++ code
-//
-void setup()
-{
-  pinMode(LED_BUILTIN, OUTPUT);
+int potPin = A0;
+int potValue;
+
+void setup() {
+  Serial.begin(9600);
 }
 
-void loop()
-{
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000); // Wait for 1000 millisecond(s)
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(1000); // Wait for 1000 millisecond(s)
+void loop() {
+  potValue = analogRead(potPin);
+
+  Serial.println(potValue);
+
+  delay(100);
 }
